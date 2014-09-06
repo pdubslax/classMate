c9aa12f2551ac2038d21dd90f1ea2af8c6b8a52c
Patrick Wilson <pdubslax@umich.edu>
2014-09-05 23:10:19 -0400

    first commit

---
+++ classMate/classMate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  classMate
+//
+//  Created by Patrick Wilson on 9/5/14.
+//  Copyright (c) 2014 Patrick Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ classMate/classMate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  classMate
+//
+//  Created by Patrick Wilson on 9/5/14.
+//  Copyright (c) 2014 Patrick Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
